68b89ca9be95f7ce7de289cabfd20468f2ddbd56
XadillaX <admin@xcoder.in>
2017-01-25 16:56:40 +0800

    feat: add cpp and pascal

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ Judger.setMiddlePath = base.setMiddlePath.bind(base);
 const c = require("./lib/compiler/c");
 Judger.setCCompilerPath = c.setCompilerPath.bind(c);
 Judger.setCCompilerOptions = c.setCompilerAdditionalOptions.bind(c);
+
+const cpp = require("./lib/compiler/c++");
+Judger.setCPPCompilerPath = cpp.setCompilerPath.bind(cpp);
+Judger.setCPPCompilerOptions = cpp.setCompilerAdditionalOptions.bind(cpp);
+
+const pascal = require("./lib/compiler/pascal");
+Judger.setPascalCompilerPath = pascal.setCompilerPath.bind(pascal);
+Judger.setPascalCompilerOptions = pascal.setCompilerAdditionalOptions.bind(pascal);
